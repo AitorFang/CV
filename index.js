5b2b8a51ce6891ac4731d47c01b8ed3fc5516357
AitorFang <66106463+AitorFang@users.noreply.github.com>
2020-10-26 16:09:50 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -8,71 +8,83 @@ $("h1").css("color", "red");
 $(document).on("scroll", function(){
     $('.menu').css({'position':'fixed', 'background-color': 'red'});
 });
-*/
- $(document).ready(function(){
-    $('.menu').css({'position':'fixed'});
+/* Al cargar el documento, fijo el menu */
+
+$(document).ready(function () {
+    $('.menu').css({ 'position': 'fixed' });
     console.log('pogno fixed el menu');
 });
 
 /* Animaciones sobre el menú */
-if ( isMobile() ) {
+if (isMobile()) {
     // código en el caso de que sea un dispositivo móvil
-    $('#botonmenu').on('click',function(){  
+    console.log("estoy desde móvil");
+    $('#botonmenu').on('click', function () {
         console.log('meto una nueva clase');
         $('#botonmenu').addClass('active-style');
-        $('#lista').slideToggle(); 
+        $('#lista').slideToggle();
         /* $('#lista').show("slide", { direction: "left" }, 6000); */
-        console.log("estoy desde móvil");
+        
     });
-   } else {
-       console.log("no estoy desde móvil");
+
+    $(".nav-link").click(function (event) {
+        /* event.preventDefault(); */
+        console.log('clck en link de laa lista');
+        $("#lista").animate({ opacity: '0.2' }).slideToggle();
+        $("#lista").animate({ opacity: '1' }); /* devuelvo valor de opacidad */
+        $("#botonmenu").delay(600).blur(); /*retorna boton a situacion inicial, no focus() */
+    });
+
+} else {
+    console.log("no estoy desde móvil");
 
     // código en el caso de que no sea un dispositivo móvil
 
-    $('#botonmenu').on('click',function(){  
+    $('#botonmenu').on('click', function () {
         console.log('meto una nueva clase');
         $('#botonmenu').addClass('active-style');
-        $('#lista').slideToggle(); 
+        $('#lista').slideToggle();
         /* $('#lista').show("slide", { direction: "left" }, 6000); */
     });
-    
-    
-    $('#lista').on('mouseleave',function(){ 
-     /*$('#lista').hide(); */
-     $("#lista").animate({opacity:'0.2'}).slideToggle();
-        $("#lista").animate({opacity:'1'}); /* devuelvo valor de opacidad */
-     $('#botonmenu').removeClass('active-style');
-     $('#botonmenu').delay(600).blur(); /*retorna boton a situacion inicial, no focus() */
-     console.log('quito una nueva clase');
-    });
-    
-    
-    $(".nav-link").click(function(){
-        $("#lista").animate({opacity:'0.2'}).slideToggle();
-        $("#lista").animate({opacity:'1'}); /* devuelvo valor de opacidad */
+
+
+    $('#lista').on('mouseleave', function () {
+        /*$('#lista').hide(); */
+        $("#lista").animate({ opacity: '0.2' }).slideToggle();
+        $("#lista").animate({ opacity: '1' }); /* devuelvo valor de opacidad */
+        $('#botonmenu').removeClass('active-style');
         $('#botonmenu').delay(600).blur(); /*retorna boton a situacion inicial, no focus() */
+        console.log('quito una nueva clase');
     });
-   }
+
+
+    $(".nav-link").click(function () {
+        $("#lista").animate({ opacity: '0.2' }).slideToggle();
+        $("#lista").animate({ opacity: '1' }); /* devuelvo valor de opacidad */
+        $("#botonmenu").delay(600).blur(); /*retorna boton a situacion inicial, no focus() */
+    });
+}
 
 
 /* Animaciones imagenes de seccion particulares */
 
-$(".imgmovrocks").on("mouseover",function(){
-        $(".imgmovrocks").animate ({
+$(".imgmovrocks").on("mouseover", function () {
+    $(".imgmovrocks").animate({
         width: '102%',
-        height:'102%'
-    },"slow");
+        height: '102%'
+    }, "slow");
 });
 
-$('.imgmovrocks').on('mouseleave',function(){
-    $('.imgmovrocks').animate ({
+$('.imgmovrocks').on('mouseleave', function () {
+    $('.imgmovrocks').animate({
         width: '100%',
-        height:'100%'
-    },'slow');
+        height: '100%'
+    }, 'slow');
 });
+
 /* Determinar si la web es visitadad desde dispositivo móvil */
 
-function isMobile(){
+function isMobile() {
     return (
         (navigator.userAgent.match(/Android/i)) ||
         (navigator.userAgent.match(/webOS/i)) ||
